606fd1cdf9992a49d3a3d757f48090ea4164ad11
sbtalk71 <sbtalk@yahoo.com>
2025-08-01 11:55:58 +0530

    AppComponent modified

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Jenkins build Application');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Jenkins new build Application');
   });
 });
